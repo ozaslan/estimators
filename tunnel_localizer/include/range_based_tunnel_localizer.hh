d85927e86aa505fa1b9a6acb8439ac25fb0706d9
Salih Tolga Ozaslan <ozaslan@seas.upenn.edu>
2015-02-22 21:16:00 -0500

    -- Modified RangeBasedTunnelLocalizer : --- updated fitness score calculcation --- integrated fitness score to covariance calculation --- removed reset function and add '_clean_start' flag in push_X functions --- cleaned ICP code --- added more informative ASSERT macros' (defined in utils.hh) --- detected some memory leak points (still not fixed) --- no longer returns non-intersecting rays for visualization --- removed get_pose function with separate pos and dcm arguments --- get_cov now has the option to integrate the fitness_scores effect --- added tolence and max_iter stop criterion to ICP --- determined the need for replacing map registration with octree registration (still not fixed) --- reverted 'static' variables for future parallelizability. --- added assertion for 'cluster_id != 0'. This is reserved for occluded, to-be-neglected rays --- heavily commented the source and header files.

--- tunnel_localizer/include/range_based_tunnel_localizer.hh
+++ tunnel_localizer/include/range_based_tunnel_localizer.hh
@@ -7,6 +7,7 @@
 #include <numeric>
 #include <iostream>
 #include <Eigen/Dense>
+#include <Eigen/Eigenvalues> 
 #include <pcl/point_types.h>
 #include <pcl/octree/octree.h>
 #include <pcl/registration/icp.h>
@@ -15,97 +16,117 @@
 
 #include <utils.hh>
 
-/*	This class gives a pose estimate for the robot in a known environment. 
-	A map point cloud is given as an argument. Also an initial estimate 
-	of the pose is expected. The user can select DOFs to updated while others 
-	are kept constant. Sensor inputs consist of laser and RGBD data. From 
-	the sensor data, it constructs a single collective point cloud structure 
-	and runs PCL's ICP. The output is the ICP pose and a 3D approxrimation 
-	of Censi's covariance estimate. */
+/*	This class provides pose estimate of the robot in a known environment.
+	The class requires the map as point cloud, an initial estimate,
+	and point cloud or scan data from sensors. There is no soft bound
+	on the total number of sensors that can be integrated, however performance 
+	may degrade with excess amount of data. For aligning the sensor data
+	with the map, 3DOF ICP is used with y-z-yaw being the free DOFs.
+	It also provides an uncertainy measure defined as in Censi's ICRA 2009 
+	paper.
+
+	This implementation is suitable for using with particle filters as well.
+	The user is supposed to push sensor data once and with different initial
+	poses carry the estimation. After each iteration pose, covariance and 
+	fitness of ICP can be fetched to vote for each particle in the wrapper
+	class.
+*/
 
 // ### I should better assume orientation to be represented as quadternions 
 // and calculate uncertainty in that space. Also this will require me to
 // return orientation in quat as well instead of dcm for the sake of 
 // consistancy.
 
+// ### Be sure that IMU orientation is applied correctly to PC.
 
 class RangeBasedTunnelLocalizer{
 private:
-	int _num_laser_pushes;	// Number of pushes since the last 'estimate_pose'
-	int _num_rgbd_pushes;	// call with a 'clear_data' flag.
-	pcl::PointCloud<pcl::PointXYZ>::Ptr _pc;						// Collective pointcloud sensor data
-	pcl::PointCloud<pcl::PointXYZ>::Ptr _pc_aligned;				// '_pc' after being aligned
-	pcl::IterativeClosestPoint<pcl::PointXYZ, pcl::PointXYZ> _icp;	// We are going to use functions of this class for pose estimation
+	int _num_laser_pushes;	// Number of pushes since the last memory clean-up
+	int _num_rgbd_pushes;	// (the same as above)
+	pcl::PointCloud<pcl::PointXYZ>::Ptr _cloud;			// Collective pointcloud sensor data
+	pcl::PointCloud<pcl::PointXYZ>::Ptr _cloud_aligned;	// '_pc' after being aligned
 	pcl::octree::OctreePointCloudSearch<pcl::PointXYZ> *_octree;
 	vector<pcl::PointXYZ> _matched_map_pts;
-	Eigen::Matrix4d _pose;  // Pose of the robot (output of ICP registration)
-	Eigen::Matrix6d _fim;	// Fisher information matrix
-	double _fitness_score;	// Wellness of the fit
+	Eigen::Matrix4d _pose;		// Pose of the robot (output of ICP registration given as homo. SE(3))
+	Eigen::Matrix6d _fim;		// Fisher information matrix
+	double _fitness_scores[3];	// average of large residuals (see the ### ??? function for detailed explanation)
+	double _max_iters;			// maximum iterations of the ICP algorithm.
+	double _yz_tol;				// translational tolerance value if updates of ICP is lower than which, iteration is stopped.
+	double _yaw_tol;			// same as '_yz_tol' for yaw.
+	// ### _skip_rays;			// number of rays to skip in ICP for performance improvement.
 
 	bool _get_covariance(const sensor_msgs::LaserScan &data, Eigen::Matrix3d &cov);
 	bool _get_covariance(const sensor_msgs::PointCloud2 &data, Eigen::Matrix6d &cov);
+	
+	// This function resets the internal flags, counters, clears the collvective 
+	// point cloud. This is called when 'push_X_data(...)' is called with 'reset' flag.
+	bool _reset();
 public:
-	RangeBasedTunnelLocalizer();
-	// This function resets the internal flags, counters, estimates, clears the
-	// collvective point cloud. If not set explicitely this function is called 
-	// after each 'estimate_pose(...)' function. 
-	bool reset();
+	RangeBasedTunnelLocalizer(int max_iter = 23, double yz_tol = 0.01, double yaw_tol = 0.035);
+	// ### Replace this with set_octree(...)
 	// This function sets the internal map pointer. Changes in the referenced 
 	// map do reflect to the internal copy of the map pointer. Thus this function
 	// call is of contant time complexity.
 	bool set_map(const pcl::PointCloud<pcl::PointXYZ>::Ptr &map);
-	// This adds laser points to the collective sensor pointcloud 
+	// ### To be implemented.
+	bool set_octree(const pcl::octree::OctreePointCloudSearch<pcl::PointXYZ> &octree_search);
+	// This adds laser points to the collective sensor pointcloud.
 	// 'rel_pose' is the pose of the laser scanner in the body frame
-	// 'mask' is a boolean vector where a 'false' denotes its corresponding 
+	// 'mask' is a char vector where '0' denotes that the corresponding 
 	// data should be negleceted. All data is used if sizes of 'ranges' 
-	// and 'mask' do not match.
-	bool push_laser_data(const Eigen::Matrix4d &rel_pose, const sensor_msgs::LaserScan &data, const vector<char> &mask, char cluster_id);
-	// This adds pointcloud, possibly from an rgbd sensor,
-	// to the collvective sensor pointcloud.
-	// 'rel_pose' is the pose of the RGBD sensor  in the body frame
-	// 'mask' is a boolean vector where a 'false' denotes its corresponding 
-	// data should be negleced. 'mask' should be indexed in row-major
-	// order. All data is used if the sizes of 'data' and 'mask' do
-	// not match.
-	bool push_rgbd_data(const Eigen::Matrix4d &rel_pose, const sensor_msgs::PointCloud2 &data, const vector<char> &mask, char cluster_id);
-	// This function run the PCL ICP function starting from the 
+	// and 'mask' do not match. Otherwise only ranges where 'mask[i] == 
+	// cluster_id' are processed. If 'clean_start' is set, '_reset()'
+	// is called before every other operation.
+	bool push_laser_data(const Eigen::Matrix4d &rel_pose, const sensor_msgs::LaserScan &data, const vector<char> &mask, char cluster_id, bool clean_start = false);
+	// This adds pointcloud, possibly from an rgbd sensor, to the 
+	// collvective sensor pointcloud. 'rel_pose' is the pose of 
+	// the RGBD sensor in the body frame. 'mask' is a char vector 
+	// where '0' denotes that the corresponding data should be 
+	// negleced. 'mask' should be indexed in row-major order. All data 
+	// is used if the sizes of 'data' and 'mask' do not match. Otherwise
+	// only ranges where 'mask[i] == cluster_id' are processed.
+	// If 'clean_start' is set, '_reset()' is called before every 
+	// other operation.
+	bool push_rgbd_data(const Eigen::Matrix4d &rel_pose, const sensor_msgs::PointCloud2 &data, const vector<char> &mask, char cluster_id, bool clean_start = false);
+	// This function runs the 3D ICP algorithm starting from the 
 	// given initial pose. 'get_pose', 'get_covariance' and 
-	// 'get_fitness_score' function are used to get results.
-	// Care should be taken due to the PCL ICP's being a black
-	// box. Precaution should be taken for possible drastically
-	// wrong pose estimates.
-	// ### Write your own ICP. Until I implement mine, I cannot
-	// constrain updates to certain coordinates.
-	bool estimate_pose(const Eigen::Matrix4d &init_pose);
+	// 'get_fitness_score' functions can be used to get the results.
+	// In this implementation only y-z-yaw DOFs are updated. This
+	// function s returns the number of iterations.
+	int estimate_pose(const Eigen::Matrix4d &init_pose);
 	// This function assumes that 'estimate_pose' function is called
 	// beforehand. When properly called, it returns the pose in 
 	// world frame. Otherwise last pose residing from the most 
 	// recent estimate is returned.
-	bool get_pose(Eigen::Matrix3d &dcm , Eigen::Vector3d &pos);
+	bool get_pose(Eigen::Matrix4d &pose);
 	// This function, when it is called after the 'estimate_pose'
-	// function, rigidly transforms the collective pointcloud to
-	// the estimated pose, then returns. It does not have any
-	// effect on the execution of the estimation process but
-	// provides material for visualization. If a rgbd data is
-	// pushed and/or laser intensities are available, pointcloud
+	// function, it returns the rigidly transforms the collective 
+	// pointcloud (obtained while running ICP) the estimated pose. 
+	// It does not have any effect on the execution of the estimation 
+	// process but provides material for visualization. ### If a rgbd 
+	// data is pushed and/or laser intensities are available, pointcloud
 	// will include color information as well.
 	bool get_registered_pointcloud(pcl::PointCloud<pcl::PointXYZ>::Ptr &pc);
 	// This estimates the covariance of the measurement model
-	// using Censi's method using the shape of the 3D point cloud.
+	// with Censi's method using the shape of the 3D point cloud.
 	// Depending on the pushed data, either 2D method or a new 3D
-	// method is used. See the related paper of Censi for the 
-	// former method. ### To be implemented ### If a 3D point
+	// method is used. See the related paper of Censi 2009 ICRA for 
+	// the former method. ### To be implemented ### If a 3D point
 	// cloud is pushed, instead of lines, planes are used 
-	// while estimation information of each ray  (yet point of
-	// the rgbd data). This function gives higher covariances
-	// if the registration fitness is low.
-	bool get_covariance(Eigen::Matrix6d &cov);
+	// in information gain of each ray. If 'apply_fitness_result = true'
+	// covariance is modified to reflect the effect of '_fitness_scores'.
+	// This always increases the covariance and gives are more 
+	// conservative estimate. 
+	bool get_covariance(Eigen::Matrix6d &cov, bool apply_fitness_result = false);
 	// This function, independent of the covariance estimate,
 	// gives a score of wellness of the fit. Higher values 
-	// mean a good fit.
-	double get_fitness_score();
-
-	bool get_sensor_map_correspondences(vector<pcl::PointXYZ> &sensor_pts, vector<pcl::PointXYZ> &map_pts);
+	// mean a worse fit. For each direction residual vectors' 
+	// corresponding component is squared and accumulated.
+	bool get_fitness_scores(double &y, double &z, double &yaw);
+	// This function returns the sensor data map point correspondences.
+	// Sensor points are at their transformed locations determined after
+	// ICP. This function is intended for visualization purposes.
+	bool get_correspondences(vector<pcl::PointXYZ> &sensor_pts, vector<pcl::PointXYZ> &map_pts);
 };
 
 
--- tunnel_localizer/src/range_based_tunnel_localizer.cc
+++ tunnel_localizer/src/range_based_tunnel_localizer.cc
@@ -5,41 +5,43 @@
 
 #include "range_based_tunnel_localizer.hh"
 
-RangeBasedTunnelLocalizer::RangeBasedTunnelLocalizer(){
-	_pc	        = pcl::PointCloud<pcl::PointXYZ>().makeShared();
-	_pc_aligned = pcl::PointCloud<pcl::PointXYZ>().makeShared();
+RangeBasedTunnelLocalizer::RangeBasedTunnelLocalizer(int max_iter, double yz_tol, double yaw_tol){
+	_cloud	       = pcl::PointCloud<pcl::PointXYZ>().makeShared();
+	_cloud_aligned = pcl::PointCloud<pcl::PointXYZ>().makeShared();
+	_max_iters = max_iter;
+	_yz_tol = yz_tol;
+	_yaw_tol = yaw_tol;
 }
 
-bool RangeBasedTunnelLocalizer::reset(){
-	_pc->points.clear();
+bool RangeBasedTunnelLocalizer::_reset(){
+	_cloud->points.clear();
+	_cloud_aligned->points.clear();
 	_num_laser_pushes = 0;
 	_num_rgbd_pushes  = 0;
-	_pose *= 0;
-	_fim *= 0;
-	_fitness_score = 0;
+	_fim = Eigen::Matrix6d::Zero();
 	return true;
 }
 
 bool RangeBasedTunnelLocalizer::set_map(const pcl::PointCloud<pcl::PointXYZ>::Ptr &map){
-	_icp.setInputTarget(map);
+	// ### This will cause memory leak!!!
 	_octree = new pcl::octree::OctreePointCloudSearch<pcl::PointXYZ>(0.05);
 	_octree->setInputCloud(map);
 	_octree->addPointsFromInputCloud();
 	return true;
 }
 
-bool RangeBasedTunnelLocalizer::push_laser_data(const Eigen::Matrix4d &rel_pose, const sensor_msgs::LaserScan &data, const vector<char> &mask, char cluster_id){
+bool RangeBasedTunnelLocalizer::push_laser_data(const Eigen::Matrix4d &rel_pose, const sensor_msgs::LaserScan &data, const vector<char> &mask, char cluster_id, bool clean_start){
 	// ### This still does not incorporate the color information
-	assert(mask.size() == 0 || data.ranges.size() == mask.size());
+	ASSERT(mask.size() == 0 || data.ranges.size() == mask.size(), "mask and data size should be the same.");
+	ASSERT(cluster_id != 0, "Cluster \"0\" is reserved.");
+
+	if(clean_start == true)
+		_reset();
 
 	// Reserve required space to prevent repetitive memory allocation
-	_pc->points.reserve(_pc->points.size() + data.ranges.size());
+	_cloud->points.reserve(_cloud->points.size() + data.ranges.size());
 
-	//cout << "-- L1" << endl;
-	//cout << _pc->points.size() << endl;
-	//cout << "cluster_id = " << (int)cluster_id << endl;
-
-	Eigen::Vector4d pt;
+	static Eigen::Vector4d pt;
 	double th = data.angle_min;
 	for(int i = 0 ; i < (int)data.ranges.size() ; i++, th += data.angle_increment){
 		if(mask.size() != 0 && mask[i] != cluster_id)
@@ -47,53 +49,44 @@ bool RangeBasedTunnelLocalizer::push_laser_data(const Eigen::Matrix4d &rel_pose,
 		utils::polar2euclidean(data.ranges[i], th, pt(0), pt(1));
 		pt(2) = pt(3) = 0;
 		pt = rel_pose * pt;
-		_pc->points.push_back(pcl::PointXYZ(pt(0), pt(1), pt(2)));
+		_cloud->points.push_back(pcl::PointXYZ(pt(0), pt(1), pt(2)));
 	}
-
-	//cout << "-- L2" << endl;
-	//cout << "# of pts in the cloud : " << _pc->points.size() << endl;
 
 	// Accumulate information due to the laser scanner onto the _fim matrix.
 	// Each additional information is in the body frame. In the 'get_covariance(...)'
-	// function (points have to be registered) this is projected onto the world 
+	// function (if points have to been registered) this is projected onto the world 
 	// frame.
 	Eigen::Matrix3d fim_xyz = Eigen::Matrix3d::Zero();			// Fisher information for x, y, z coords.
 	Eigen::Matrix3d dcm		= rel_pose.topLeftCorner<3, 3>();	// Rotation matrix
 	Eigen::Matrix3d fim_xyp;									// Fisher information for x, y, yaw
 	double fi_p = 0;											// Fisher information for yaw only (neglecting correlative information)
 	
-	//cout << "-- L3" << endl;
 	utils::get_fim(data, mask, fim_xyp, cluster_id);
 
-	//cout << fim_xyp << endl;
-	//cout << fim_xyp.inverse() << endl;
-	
-	//cout << "-- L4" << endl;
 	fim_xyz.topLeftCorner<2, 2>() = fim_xyp.topLeftCorner<2, 2>();
 	
-	//cout << "-- L5" << endl;
 	fim_xyz = dcm.transpose() * fim_xyz * dcm;
 	
-	//cout << "-- L6" << endl;
-
 	fi_p = fim_xyp(2, 2) * dcm(2, 2);
 
 	_fim.block<3, 3>(0, 0) += fim_xyz;
 	_fim(3, 3) += fi_p;
 
-
-	//cout << "-- L7" << endl;
-
 	_num_laser_pushes++;
+
 	return true;
 }
 
-bool RangeBasedTunnelLocalizer::push_rgbd_data(const Eigen::Matrix4d &rel_pose, const sensor_msgs::PointCloud2 &data, const vector<char> &mask, char m){
+bool RangeBasedTunnelLocalizer::push_rgbd_data(const Eigen::Matrix4d &rel_pose, const sensor_msgs::PointCloud2 &data, const vector<char> &mask, char cluster_id, bool clean_start){
 	// ### This still does not incorporate the color information
-	assert(mask.size() == 0 || data.data.size() == mask.size());
+	ASSERT(mask.size() == 0 || data.data.size() == mask.size(), "mask and data size should be the same.");
+	ASSERT(cluster_id == 0, "Cluster \"0\" is reserved.");
+
+	if(clean_start == true)
+		_reset();
 
 	// Reserve required space to prevent repetitive memory allocation
-	_pc->points.reserve(_pc->points.size() + data.data.size());
+	_cloud->points.reserve(_cloud->points.size() + data.data.size());
 
 	Eigen::Vector4d pt;
 	
@@ -108,154 +101,142 @@ bool RangeBasedTunnelLocalizer::push_rgbd_data(const Eigen::Matrix4d &rel_pose, 
 	return true;
 }
 
-bool RangeBasedTunnelLocalizer::estimate_pose(const Eigen::Matrix4d &init_pose){
-	
-	// ### This part might need some optimization.
-	// If this class is used with PF, then according to previous
-	// transformations, new transformations can be concatenated 
-	// easily and efficiently without copying the whole data
-	//cout << "-- R0" << endl;
-	//static pcl::PointCloud<pcl::PointXYZ>::Ptr pc_transformed = pcl::PointCloud<pcl::PointXYZ>().makeShared();
-	_pc_aligned = pcl::PointCloud<pcl::PointXYZ>().makeShared();
-	//pcl::copyPointCloud(*_pc, *_pc_aligned);
-	pcl::transformPointCloud(*_pc, *_pc_aligned, init_pose);
-	/*
-	_icp.setInputSource(pc_transformed);
-	_icp.align(*_pc_aligned);
-	_pose = _icp.getFinalTransformation().cast<double>();
-	_fitness_score = _icp.getFitnessScore();
-	return _icp.hasConverged();
-	*/
+int RangeBasedTunnelLocalizer::estimate_pose(const Eigen::Matrix4d &init_pose){
+	// ### This will cause memory leak!!!
+	_cloud_aligned = pcl::PointCloud<pcl::PointXYZ>().makeShared();
+	// Initialize '_cloud_aligned' by transforming the collective point cloud
+	// with the initial pose.
+	pcl::transformPointCloud(*_cloud, *_cloud_aligned, init_pose);
 
-	// For the time being I start using my own implemetation for updating only
-	// y, z and psi since PCL::ICP does not allow fixing DOFs. Usually IMU's
-	// roll, pitch estimate is acceptably reliable, however ICP cannot constrain
-	// updates along there axis.
-	
-	// (1) - For each ray, find the point of intersection. Use ray-casting of PCL
-	// (2) - Prepare the matrix A(2*i,:) = [p_y, p_z, 1, 0] and A(2*i+1,:) = [p_z, -p_y, 0, 1].
-	// and the vector b(:) = [r_y, r_z] where p's are the collective sensor data 
-	// points and r's are the results of ray casting.
+	// Steps : 
+	// (1) - For each ray, find the point of intersection. Use ray-casting of PCL-Octree
+	// (2) - Prepare the matrix A and vector b s.t.  A*x = b
 	// (3) - Solve for A*x = b where x = [cos(th) sin(th), dy, dz]'.
-	// (4) - Repeat until b's are small enough.
+	// (4) - Repeat for _max_iter's or tolerance conditions are met
 	// (*) - Use weighing to eliminate outliers.
-	int num_pts = _pc_aligned->points.size();
-	int num_valid_pts = 0;
-	double dTz = 0;
+	int num_pts = _cloud_aligned->points.size();
+	int num_valid_pts = 0;	// # of data points intersecting with the map.
+	double dTz = 0;			// update along the z-coordinate.
+
 	Eigen::MatrixXd A(num_pts, 3);
 	Eigen::VectorXd b(num_pts);
-	Eigen::Vector3d x;
-	Eigen::Vector3f origin, r;
-	Eigen::Vector3d rpy;
-	Eigen::Matrix4d curr_pose = init_pose;
-	origin(0) = init_pose(0, 3);
-	origin(1) = init_pose(1, 3);
-	origin(2) = curr_pose(2, 3);	
+	static Eigen::Vector3d x, rpy;	// solution to Ax=b, 
+									// roll-pitch-yaw
+	static Eigen::Vector3f pos;	// initial position of the robot.
+								// This is given as argument to ray-caster.
+
+	Eigen::Matrix4d curr_pose = init_pose; // 'current' pose after each iteration.
+
+	// container for ray-casting results :
+	static pcl::octree::OctreePointCloudSearch<pcl::PointXYZ>::AlignedPointTVector voxel_centers;
+
+	pos = curr_pose.topRightCorner<3, 1>().cast<float>();
 
 	_matched_map_pts.resize(num_pts);
-	int max_iter = 8;
-	for(int iter = 0 ; iter < max_iter ; iter++){
+
+	int iter;
+	for(iter = 0 ; iter < _max_iters ; iter++){
 		num_valid_pts = 0;
+		_fitness_scores[0] = _fitness_scores[1] = _fitness_scores[2] = 0;
 		for(int i = 0 ; i < num_pts ; i++){
-			//cout << "i" << i << endl;
-			r(0) = _pc_aligned->points[i].x - origin(0);
-			r(1) = _pc_aligned->points[i].y - origin(1);
-			r(2) = _pc_aligned->points[i].z - origin(2);
-			A(i, 0) =  r(1);
-			A(i, 1) = -r(0);
+			// Get the ray in the body frame ()
+			static Eigen::Vector3f ray;
+			ray(0) = _cloud_aligned->points[i].x - pos(0);
+			ray(1) = _cloud_aligned->points[i].y - pos(1);
+			ray(2) = _cloud_aligned->points[i].z - pos(2);
+			A(i, 0) =  ray(1);
+			A(i, 1) = -ray(0);
 			A(i, 2) =  1;
-			//cout << origin << endl;
-			//cout << direction << endl;
-			pcl::octree::OctreePointCloudSearch<pcl::PointXYZ>::AlignedPointTVector voxel_centers;
-				_octree->getIntersectedVoxelCenters(origin, r,	voxel_centers , 0);
-			//cout << "# of intr. voxels : " << voxel_center_centers.size() << endl;
+			// Fetch only the first intersection point
+			_octree->getIntersectedVoxelCenters(pos, ray, voxel_centers , 1);
+			// If there is no intersection, nullify the effect by zero'ing the corresponding equation
 			if(voxel_centers.size() == 0){
 				A.block<1, 3>(i, 0) *= 0;
-				b(i)     = 0;
-				_matched_map_pts[i].x = std::numeric_limits<float>::infinity();
-				_matched_map_pts[i].y = std::numeric_limits<float>::infinity();
-				_matched_map_pts[i].z = std::numeric_limits<float>::infinity();
+				b(i)				 = 0;
+				_matched_map_pts[i].x = 
+					_matched_map_pts[i].y = 
+					_matched_map_pts[i].z = std::numeric_limits<float>::infinity();
 			} else {
-				int num_inter = voxel_centers.size();
-				pcl::PointXYZ &inter = voxel_centers[num_inter - 1];
-				//cout << "# of inter. voxels : " << num_inter << endl;
-				
-				_matched_map_pts[i] = inter;
-				b(i) = inter.y - origin(1);
-				double ddTz = inter.z - _pc_aligned->points[i].z;
-				dTz += ddTz * exp(-r(2) / r.norm());
-				num_valid_pts++;
-
-				Eigen::Vector3d w_vec;
-				w_vec(0) = _pc_aligned->points[i].x - inter.x;
-				w_vec(1) = _pc_aligned->points[i].y - inter.y;
-				w_vec(2) = _pc_aligned->points[i].z - inter.z;
-				double weight = exp(-pow(w_vec.squaredNorm(), 1.5));
+				// Save the matched point
+				_matched_map_pts[i] = voxel_centers[0];
+				// Use only the y-comp of the residual vector. Because 
+				// this is going to contribute to y and yaw updates only.
+				b(i) = voxel_centers[0].y - pos(1);
+				// Get the residual vector 
+				static Eigen::Vector3d res_vec;
+				res_vec(0) = _cloud_aligned->points[i].x - voxel_centers[0].x;
+				res_vec(1) = _cloud_aligned->points[i].y - voxel_centers[0].y;
+				res_vec(2) = _cloud_aligned->points[i].z - voxel_centers[0].z;
+				// Update the delta-z-estimate according to the direction of the 
+				// corresponding ray's z component. The update factor is weighed using
+				// ML outlier elimination.
+				dTz += -res_vec(2) * (exp(fabs(ray(2) / ray.norm())) - 1);
+				// Calculate a weighing coefficent for ML outlier elimination
+				// regarding y-yaw DOFs
+				double weight = exp(-pow(res_vec.squaredNorm(), 1.5));
 				b(i) *= weight;
 				A.block<1, 3>(i, 0) *= weight;
 
+				num_valid_pts++;
 
-				//double wy = voxel_centers[0].y - _pc_aligned->points[i].y;
-				//double wz = voxel_centers[0].z - _pc_aligned->points[i].z;
-				//double w  = 1/sqrt(wz * wz + wy * wy + 0.00001);
-				//A.block<2, 4>(2*i, 0) *= w;
+				_fitness_scores[0] += res_vec[1] * res_vec[1];
+				_fitness_scores[1] += res_vec[2] * res_vec[2];
+				_fitness_scores[2] += res_vec[1] * res_vec[1] * ray(0) * ray(0);
 			}
 		}
-		//cout << A << endl;
-		//cout << b << endl;
-		cout << "iter = " << iter << endl;
-		cout << "x : " << endl;
-		x = (A.transpose() * A).inverse() * A.transpose() * b;
-		cout << x << endl;
 
+		// Solve for the least squares solution.
+		x = (A.transpose() * A).inverse() * A.transpose() * b;
+
+		// Get the mean of dTz since it has been accumulated
 		dTz /= num_valid_pts;
+		_fitness_scores[0] /= num_valid_pts;
+		_fitness_scores[1] /= num_valid_pts;
+		_fitness_scores[2] /= num_valid_pts;
+
+		// x = [cos(yaw) sin(yaw) dY]
 		x(0) = fabs(x(0)) > 1 ? x(0) / fabs(x(0)) : x(0);
-		//double dyaw = -acos(x(0));
 		double dyaw = -1.2 * atan2(x(1), x(0));
-		cout << "dyaw = " << dyaw << endl;
 
 		// Update the position
-		origin(1) += x(2);
-		origin(2) += dTz;
+		pos(1) += x(2); // y-update 
+		pos(2) += dTz;	// z-update
 
 		// Convert the orientation to 'rpy', update yaw and 
 		// convert back to dcm.
-		Eigen::Matrix3d dcm = curr_pose.topLeftCorner(3, 3);
+		Eigen::Matrix3d dcm = curr_pose.topLeftCorner<3, 3>();
 		rpy = utils::dcm2rpy(dcm);
 		rpy(2) += dyaw;
 		dcm = utils::rpy2dcm(rpy);
 
 		// Update the global pose matrix.
 		curr_pose.topLeftCorner(3, 3) = dcm;
-		curr_pose(1, 3) = origin(1);
-		curr_pose(2, 3) = origin(2);
+		curr_pose(1, 3) = pos(1);
+		curr_pose(2, 3) = pos(2);
 	
-		cout << "curr_pose : " << curr_pose << endl;
-	
-		// Transform points.
-		pcl::transformPointCloud(*_pc, *_pc_aligned, curr_pose);
+		// Transform points for next iteration.
+		pcl::transformPointCloud(*_cloud, *_cloud_aligned, curr_pose);
 
-		//if(fabs(dTz) < 0.01 && fabs(x(2)) < 0.01 && fabs(dyaw) < 1.0/180 * PIi)
-			
+		if(fabs(x(2)) < _yz_tol && fabs(dTz) < _yz_tol && fabs(dyaw) < _yaw_tol)
+			break;
 	}
 
 	_pose = curr_pose;
 
-	return true;
+	return iter;
 }
 
-bool RangeBasedTunnelLocalizer::get_pose(Eigen::Matrix3d &dcm , Eigen::Vector3d &pos){
-	dcm = _pose.topLeftCorner<3, 3>().cast<double>();
-	pos = _pose.topRightCorner<3, 1>().cast<double>();
+bool RangeBasedTunnelLocalizer::get_pose(Eigen::Matrix4d &pose){
+    pose = _pose.cast<double>();
 	return true;
 }
 
 bool RangeBasedTunnelLocalizer::get_registered_pointcloud(pcl::PointCloud<pcl::PointXYZ>::Ptr &pc){	
-	pc = _pc_aligned;
+	pc = _cloud_aligned;
 	return true;
 }
 
-bool RangeBasedTunnelLocalizer::get_covariance(Eigen::Matrix6d &cov){
+bool RangeBasedTunnelLocalizer::get_covariance(Eigen::Matrix6d &cov, bool apply_fitness_result){
 	// ### I have to find a way to project uncertainties in orientation
 	// to other frame sets.
 	// ### I might have to fix some elements before inverting
@@ -263,18 +244,47 @@ bool RangeBasedTunnelLocalizer::get_covariance(Eigen::Matrix6d &cov){
 		if(fabs(_fim(i, i)) < 0.01)
 			_fim(i, i) = 0.01;
 	}
-	cov = _fim.inverse();
+
+	// ### This assumes uniform covariance for each rays, and superficially
+	// handles the uncertainties. I should first run ICP and then
+	// find '_fim' and so on. This will require a lot of bookkeeping etc.
+	// Thus leaving to a later version :)
+	if(apply_fitness_result){
+		Eigen::EigenSolver<Eigen::Matrix6d> es;
+		es.compute(_fim, true);
+		Eigen::MatrixXcd D = es.eigenvalues().asDiagonal();
+		Eigen::MatrixXcd V = es.eigenvectors();
+		// ### Is the rotation ypr/rpy ???
+		// We assume that 0.001 m^2 variance is perfect fit, or unit information.
+		D(1, 1) /= exp(_fitness_scores[0] / 0.001);
+		D(2, 2) /= exp(_fitness_scores[1] / 0.001);
+		D(3, 3) /= exp(_fitness_scores[2] / 0.001);
+		cov =  (V.transpose() * D.inverse() * V).real();
+	} else
+		cov = _fim.inverse();
 	return true;
 }
 
-double RangeBasedTunnelLocalizer::get_fitness_score(){
-	return _fitness_score;
+bool RangeBasedTunnelLocalizer::get_fitness_scores(double &y, double &z, double &yaw){
+	y   = _fitness_scores[0];
+	z   = _fitness_scores[1];
+	yaw = _fitness_scores[2];
+	return true;
 }
 
-bool RangeBasedTunnelLocalizer::get_sensor_map_correspondences(vector<pcl::PointXYZ> &sensor_pts, vector<pcl::PointXYZ> &map_pts){
-	map_pts = _matched_map_pts;
-	sensor_pts.resize(_pc_aligned->points.size());
-	for(int i = 0 ; i < (int)_pc_aligned->points.size() ; i++)
-		sensor_pts[i] = _pc_aligned->points[i];
+bool RangeBasedTunnelLocalizer::get_correspondences(vector<pcl::PointXYZ> &sensor_pts, vector<pcl::PointXYZ> &map_pts){
+	int num_valid_pts = 0;
+	for(int i = 0 ; i < (int)_matched_map_pts.size() ; i++)
+		if(isfinite(_matched_map_pts[i].y))
+			num_valid_pts++;
+	map_pts.resize(num_valid_pts);
+	sensor_pts.resize(num_valid_pts);
+	for(int i = 0, j = 0 ; i < (int)_cloud_aligned->points.size() ; i++){
+		if(isfinite(_matched_map_pts[i].y)){
+			sensor_pts[j] = _cloud_aligned->points[i];
+			map_pts[j] = _matched_map_pts[i];
+			j++;
+		}
+	}
 	return true;
 }
