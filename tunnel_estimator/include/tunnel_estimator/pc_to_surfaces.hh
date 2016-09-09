da9f9acf8d21a16aa259ce3ca65308ab91e430d3
ozaslan <ozaslan@seas.upenn.edu>
2016-09-09 00:26:13 -0400

    Modification on tunnel estimator

--- tunnel_estimator/include/tunnel_estimator/pc_to_surfaces.hh
+++ tunnel_estimator/include/tunnel_estimator/pc_to_surfaces.hh
@@ -3,15 +3,18 @@
 
 #include <vector>                                                                                                                                                                                              
 #include <iostream>
- 
+
 #include <pcl/io/pcd_io.h>
 #include <pcl/point_types.h>
- 
+
 #include <pcl/conversions.h>
 #include <pcl_conversions/pcl_conversions.h>
 #include <pcl/registration/ndt.h>
 #include <pcl/registration/icp.h>
 #include <pcl/registration/icp_nl.h>
+
+#include <pcl/features/normal_3d.h>
+
 // ### #include <pcl/registration/gicp.h>
 // #include <pcl/filters/approximate_voxel_grid.h>
 // #include <pcl/filters/voxel_grid.h>
@@ -27,36 +30,69 @@
 
 #include <utils.hh>
 
-class PC2Surfaces{
+struct PC2SurfacesParams{
+  public:
+    PC2SurfacesParams(){
+      max_inner_iter = 33;
+      max_outer_iter = 33;
+      outlier_id = -99999;
+      unclassified_id = 99999;
+      term_perc_crit = 0.01;
+      normal_dev_thres1 = 5; // degrees
+      normal_dev_thres2 = 5; 
+      contour_fit_thres = 0.05; // ratio to radius
+      sphere_r = 7.5; // meters
+      segment_len = 1; // meters
+      normal_search_radius = 0.25;
+      contour_type = "circle";
+    }
 
+    int max_inner_iter;
+    int max_outer_iter;
+    int outlier_id;
+    int unclassified_id;
+    double term_perc_crit;
+    double normal_dev_thres1; // degrees
+    double normal_dev_thres2; 
+    double contour_fit_thres; // ratio to radius
+    double sphere_r; // meters
+    double segment_len; // meters
+    double normal_search_radius;
+    string contour_type;
+};
+
+class PC2Surfaces{
   private:
-    int _max_inner_iter = 33;
-    int _max_outer_iter = 33;
-    int _outlier_id = -99999;
-    int _unclassified_id = 99999;
-    double _term_perc_crit = 0.01;
-    double _normal_dev_thres1 = 5; // degrees
-    double _normal_dev_thres2 = 5; 
-    double _cylinder_fit_thres = 0.05; // ratio to radius
-    double _sphere_r = 7.5; // meters
-    double _segment_len = 1; // meters
-    double _normal_search_radius = 0.25;
-    
+    PC2SurfacesParams _params;
+
     vector<int> _segment_ids;
     std::map<int, Eigen::Matrix3d> _segment_triad_map;
     std::map<int, Eigen::Vector3d> _segment_origin_map;
+    std::map<int, Eigen::VectorXd> _segment_contour_map;
     pcl::PointCloud<pcl::PointXYZ>::Ptr _pc_orig;
     pcl::PointCloud<pcl::PointXYZ>::Ptr _pc_sphere;
     pcl::PointCloud<pcl::Normal>::Ptr _pc_sphere_normals;
     pcl::NormalEstimation<pcl::PointXYZ, pcl::Normal> _ne;
+    vector<Eigen::Vector3d> _pc_projections; // y-z components are plane coordinates
 
-    bool _fit_segment(int seg);
+    // This function fits surface normals to '_pc_sphere' and returns the
+    // number of normal vectors
+    int _fit_normals();
+    // 
+    int _init_triad(int seg);
+    int _fit_segment(int seg);
+    int _filter_segment(int seg);
+    int _eliminate_outliers(int seg, const std::string &method);
+    int _project_pc(int seg);
+    int _fit_contour(int seg);
   public:
     PC2Surfaces();
-    
-    int push_pc(const pcl::PointCloud<pcl::PointXYZ>::ptr &pc);
+    PC2Surfaces(const PC2SurfacesParams &params);
+
+    int push_pc(const pcl::PointCloud<pcl::PointXYZ>::Ptr &pc);
     int fit_surfaces();
 };
+
 
 
 #endif
--- tunnel_estimator/src/pc_to_surfaces.cc
+++ tunnel_estimator/src/pc_to_surfaces.cc
@@ -1,4 +1,4 @@
-#include "tunnel_estimator/pc_to_surfaces.hh"
+#include "pc_to_surfaces.hh"
 
 #define SGN(x) ((x) == 0 ? 0 : ((x) < 0 ? -1 : +1))
 
@@ -7,20 +7,20 @@ PC2Surfaces::PC2Surfaces(){
 
 }
 
-int PC2Surface::push_pc(const pcl::PointCloud<pcl::PointXYZ>::ptr &pc){
+int PC2Surfaces::push_pc(const pcl::PointCloud<pcl::PointXYZ>::Ptr &pc){
 
   _pc_orig = pc;
   // Filter the original PC to only points that are at most 'r' distance from the the sensor origin
-  _filter_segment(_unclassified_id);
+  _filter_segment(_params.unclassified_id);
   // Fit normals to '_pc_sphere'
-  _fit_normals(_unclassified_id);
+  _fit_normals();
   // Estimate an initial triad using all the normals
-  _estimate_triad(_unclassified_id);
+  _init_triad(_params.unclassified_id);
 
   _filter_segment(0);
   _fit_segment(0);
 
-  for(int seg = 0 ; seg <= _sphere_r/_segment_len ; seg++){
+  for(int seg = 0 ; seg <= _params.sphere_r / _params.segment_len ; seg++){
     _filter_segment(seg);
     _fit_segment(seg);
     _filter_segment(-seg);
@@ -40,73 +40,121 @@ int PC2Surfaces::_fit_normals(){
   _ne.setSearchMethod (tree);
 
   // Use all neighbors in a sphere of radius 3cm
-  _ne.setRadiusSearch (_normal_search_radius);
+  _ne.setRadiusSearch (_params.normal_search_radius);
 
   // Compute the features
   _ne.compute (*_pc_sphere_normals);
+
+  return _pc_sphere_normals->size();
 }
 
-bool PC2Surfaces::_filter_segment(int seg){
+int PC2Surfaces::_filter_segment(int seg){
   Eigen::Matrix3d triad;
   Eigen::Vector3d origin;
 
-  if(seg == _unclassified_id){
-    int num_pts = _pc_orig.size();
-    _pc_sphere.reserve(num_pts / 3);
+  if(seg == _params.unclassified_id){
+    int num_pts = _pc_orig->size();
+    _pc_sphere->reserve(num_pts / 3);
+    double r_squared = pow(_params.sphere_r, 2);
     for(int i = 0 ; i < num_pts ; i++){
-      pcl::PointXYZ pt = _pc_origs[i];
-      if(pt.x * pt.x + pt.y * pt.y + pt.z * pt.z <= _sphere_r * _sphere_r)
-        _pc_sphere.push_back(pt);
+      pcl::PointXYZ pt = (*_pc_orig)[i];
+      if(pt.x * pt.x + pt.y * pt.y + pt.z * pt.z <= r_squared)
+        _pc_sphere->push_back(pt);
     }
     _segment_ids.clear();
-    _segment_ids.resize(_pc_sphere.size(), _unclassified_id);
+    _segment_ids.resize(_pc_sphere->size(), _params.unclassified_id);
 
     return true;
 
   } else if(seg == 0){
-    triad  = _segment_triad_map[_unclassified_id];
-    origin = _segment_origin_map[_unclassified_id];
+    triad  = _segment_triad_map[_params.unclassified_id];
+    origin = _segment_origin_map[_params.unclassified_id];
   } else {
-    triad  =  _segment_triad_map[seg + SGN(seg)];
-    origin = _segment_origin_map[seg + SGN(seg)];
-    origin = origin + 2 * triad.topLeftCorner<3, 1>() * _segment_len;
+    triad  =  _segment_triad_map[seg - SGN(seg)];
+    origin = _segment_origin_map[seg - SGN(seg)];
+    origin = origin + 2 * SGN(seg) * _params.segment_len * triad.topLeftCorner<3, 1>(); 
   }
 
-  int num_pts = _pc_sphere.size();
+  int num_pts = _pc_sphere->size();
   for(int i = 0 ; i < num_pts ; i++){
-    pcl::PointXYZ pt = _pc_sphere[i];
-    pt = triad(pt - origin);
-    if(-_segment_len < pt.x && pt.x <= _segment_len)
+    pcl::PointXYZ pt = (*_pc_sphere)[i];
+    pt.x -= origin(0);
+    if(-_params.segment_len < pt.x && pt.x <= _params.segment_len)
       _segment_ids[i] = seg;
   }
 
   return true;
 }
 
-bool PC2Surfaces::_fit_segment(int seg){
+int PC2Surfaces::_project_pc(int seg){
+  int num_pts = _pc_sphere->size();
 
-  bool success = false
+  Eigen::Matrix3d triad  = _segment_triad_map[seg];
+  Eigen::Vector3d origin = _segment_origin_map[seg];
 
-    for(int outer_iter = 0 ; !success && outer_iter < _max_outer_iter ; outer_iter++)
-      success = false;
-  for(int inner_iter = 0 ; !success && inner_iter < _max_inner_iter ; inner_iter++){
-    _estimate_triad(0);
-    success = !_eliminate_outliers(0, "normals");
-  }
+  if(_pc_projections.size() != _pc_sphere->size())
+    _pc_projections.resize(_pc_sphere->size());
 
-  _project_pc(0);
-
-  success = false;
-  for(int inner_iter = 0 ; !success && inner_iter < _max_inner_iter ; inner_iter++){
-    _fit_contour(0);
-    success = i!_eliminate_outliers(0, "surface");
+  for(int i = 0 ; i < num_pts ; i++){
+    if(_segment_ids[i] != seg)
+      continue;
+    pcl::PointXYZ pt = (*_pc_sphere)[i];
+    _pc_projections[i] << pt.x, pt.y, pt.z;
+    _pc_projections[i] = triad * (_pc_projections[i] - origin);
   }
 }
 
-return success;
+int PC2Surfaces::_fit_segment(int seg){
+
+  bool success = false;
+
+  for(int outer_iter = 0 ; !success && outer_iter < _params.max_outer_iter ; outer_iter++){
+    success = false;
+    for(int inner_iter = 0 ; !success && inner_iter < _params.max_inner_iter ; inner_iter++){
+      _init_triad(seg);
+      success = !_eliminate_outliers(seg, "normals");
+    }
+
+    _project_pc(seg);
+
+    success = false;
+    for(int inner_iter = 0 ; !success && inner_iter < _params.max_inner_iter ; inner_iter++){
+      _fit_contour(seg);
+      success = !_eliminate_outliers(0, "contour");
+    }
+  }
 }
 
-bool PC2Surfaces::_estimate_triad(int seg){
+int PC2Surfaces::_fit_contour(int seg){
+  if(type == _params.contour_type){
+    int num_pts = 0;
+    for(int segment : _segment_ids)
+      num_pts += segment == seg;
+    Eigen::MatrixXd A(num_pts, 3);
+    Eigen::VectorXd b(num_pts);
+    num_pts = _pc_projections.size();
+    for(int i = 0 ; i < num_pts ; i++){
+      double y, z;
+      y = _pc_projections[i](1);
+      z = _pc_projections[i](2);
+      A.row(i) << y, z, 1;
+      b(i) = -(y * y + z * z);
+    }
+
+    Eigen::VectorXd soln = (A.transpose() * A).inverse() * A.transpose() * b; 
+    double xc = - soln(0) / 2;
+    double yc = - soln(1) / 2;
+    double R  = sqrt((soln(0) * soln(0) + soln(1) * soln(1)) / 4 - soln(2));
+
+    _segment_contour_map[seg] << xc, yc, R; 
+
+    return 3;
+  } else {
+    return 0;
+  }
+}
+
+int PC2Surfaces::_init_triad(int seg){
 
   int num_pts = 0;
   for(int ind : _segment_ids)
@@ -114,23 +162,24 @@ bool PC2Surfaces::_estimate_triad(int seg){
 
   Eigen::MatrixXd normals(num_pts, 3);
 
-  num_pts = _pc_sphere.size();
+  num_pts = _pc_sphere->size();
   for(int i = 0, j = 0 ; i < num_pts ; i++){
     if(_segment_ids[i] != seg)
       continue;
-    normals(j  , 0) = _pc_sphere_normals[i].x;
-    normals(j  , 1) = _pc_sphere_normals[i].y;
-    normals(j++, 2) = _pc_sphere_normals[i].z;
+    pcl::Normal pc_normal = (*_pc_sphere_normals)[i];
+    normals(j  , 0) = pc_normal.data_c[0];
+    normals(j  , 1) = pc_normal.data_c[1];
+    normals(j++, 2) = pc_normal.data_c[2];
   }
 
-  EigenSolver<MatrixXd> es(normals.transpose() * normals);
+  Eigen::SelfAdjointEigenSolver<Eigen::Matrix3d> es(Eigen::Matrix3d(normals.transpose() * normals));
 
-  int min_eval_ind;
-  es.eigenvalues().minCoeff(min_eval_ind, NULL);
+  Eigen::Vector3d::Index min_eval_ind;
+  es.eigenvalues().minCoeff(&min_eval_ind);
 
   Eigen::Matrix3d triad;
 
-  triad.col(0) = es.eigenvectors.col(min_eval_ind);
+  triad.col(0) = es.eigenvectors().col(min_eval_ind);
   triad.col(1) << 0, 0, 1;
   triad.col(1) = triad.col(0).cross(triad.col(1));
   triad.col(2) = triad.col(0).cross(triad.col(1));
@@ -139,5 +188,39 @@ bool PC2Surfaces::_estimate_triad(int seg){
 }
 
 int PC2Surfaces::_eliminate_outliers(int seg, const std::string &method){
-
+  int num_pts = _pc_sphere->size();
+  int num_eliminated = 0;
+  if(method == "normals"){
+    Eigen::Vector3d segment_axis = _segment_triad_map[seg].topLeftCorner<3, 1>();
+    Eigen::Vector3d normal;
+    for(int i = 0 ; i < num_pts ; i++){
+      if(_segment_ids[i] == seg){
+        pcl::Normal &pcl_normal = (*_pc_sphere_normals)[i];
+        normal << pcl_normal.data_c[0], pcl_normal.data_c[1], pcl_normal.data_c[2];
+        double dot_prod = normal.dot(segment_axis);
+        if(acos(dot_prod) > _params.normal_dev_thres1){
+          _segment_ids[i] = _params.outlier_id;
+          num_eliminated++;
+        }
+      }
+    }
+  } else if(method == "contour"){
+    if(_params.contour_type == "circle"){
+      Eigen::VectorXd &contour_fit = _segment_contour_map[seg];
+      double xc = contour_fit(0);
+      double yc = contour_fit(1);
+      double R  = contour_fit(2);
+      for(int i = 0 ; i < num_pts ; i++){
+        if(_segment_ids[i] == seg){
+          Eigen::Vector3d &proj_pt = _pc_projections[i];
+          double r = sqrt(pow(proj_pt(1) - xc, 2) + pow(proj_pt(2) - yc, 2));
+          if( fabs(R - r) / R > _params.contour_fit_thres){
+            _segment_ids[i] = _params.outlier_id;
+            num_eliminated++;
+          }
+        }
+      }
+    }
+  }
+  return num_eliminated;
 }
